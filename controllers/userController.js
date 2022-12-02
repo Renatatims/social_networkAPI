6c6df76605477b36be6d4e85351715336d4778a8
ReTims <renatatims@gmail.com>
2022-12-02 12:37:42 -0600

    Added getSingleUser and createUser to userController + added routes to userRoutes

--- controllers/userController.js
+++ controllers/userController.js
@@ -16,5 +16,29 @@ module.exports = {
           return res.status(500).json(err);
         });
     },
+  // Get a single user
+  getSingleUser(req, res) {
+    User.findOne({ _id: req.params.userId })
+      .select('-__v')
+      .populate("thoughts")
+      .populate("friends")
+      .then(async (user) =>
+        !user
+          ? res.status(404).json({ message: 'No user with that ID' })
+          : res.json({
+              user
+            })
+      )
+      .catch((err) => {
+        console.log(err);
+        return res.status(500).json(err);
+      });
+  },
+  // create a new user
+  createUser(req, res) {
+    User.create(req.body)
+      .then((user) => res.json(user))
+      .catch((err) => res.status(500).json(err));
+  },
 
 }
