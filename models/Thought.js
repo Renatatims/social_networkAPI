9cb258e23dcd7bafa9ebf6691cdefa831408c9bb
ReTims <renatatims@gmail.com>
2022-12-02 11:54:26 -0600

    Created 2 virtuals: friendCount on User.js model and reactionCOunt on Thought model + added reactionSchema to Thought.js and now the schema and models were created in MongoDB compass

--- models/Thought.js
+++ models/Thought.js
@@ -1,4 +1,5 @@
 const { Schema, model } = require("mongoose");
+const reactionSchema = require("./Reaction");
 
 const thoughtSchema = new Schema(
   {
@@ -27,6 +28,10 @@ const thoughtSchema = new Schema(
     id: false,
   }
 );
+
+thoughtSchema.virtual("reactionCount").get(function (){
+  return this.reactions.length;
+});
 
 const Thought = model("thought", thoughtSchema);
 
