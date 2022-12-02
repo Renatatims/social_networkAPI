e14a44df63fcdcaf86c25c4ab9b7f18961e8452f
ReTims <renatatims@gmail.com>
2022-12-02 16:02:33 -0600

    Added getSingleThought and createThought to thoughtController + thoughtRoute.js added the routes

--- controllers/thoughtController.js
+++ controllers/thoughtController.js
@@ -7,4 +7,25 @@ module.exports = {
       .then((thoughts) => res.json(thoughts))
       .catch((err) => res.status(500).json(err));
   },
+  // Get a single thought
+  getSingleThought(req, res) {
+    Thought.findOne({ _id: req.params.thoughtId })
+      .then(async (thought) =>
+        !thought
+          ? res.status(404).json({ message: 'No thought with that ID' })
+          : res.json({
+              thought
+            })
+      )
+      .catch((err) => {
+        console.log(err);
+        return res.status(500).json(err);
+      });
+  },
+  // create a new thought
+  createThought(req, res) {
+    Thought.create(req.body)
+      .then((thought) => res.json(thought))
+      .catch((err) => res.status(500).json(err));
+  },
 }
--- routes/api/thoughtRoutes.js
+++ routes/api/thoughtRoutes.js
@@ -1,10 +1,16 @@
 const router = require("express").Router();
 const {
   getThoughts,
+  getSingleThought,
+  createThought
+
+
 } = require("../../controllers/userController");
 
 // /api/thoughts
-router.route("/").get(getThoughts);
+router.route("/").get(getThoughts).post(createThought);
 
+// /api/thoughts/:thoughtId
+router.route("/:userId").get(getSingleThought);
 
-module.exports = router;
+module.exports = router; 
