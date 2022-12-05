621d39c0bc9bb4e3d3c654174d04918e28e2ec17
ReTims <renatatims@gmail.com>
2022-12-05 11:33:28 -0600

    Added e-mail validation to user model

--- models/Reaction.js
+++ models/Reaction.js
@@ -19,7 +19,7 @@ const reactionSchema = new Schema(
     createdAt: {
       type: Date,
       default: Date.now,
-      get: (timestamp) => dayjs(timestamp).format('{YYYY} MM-DDTHH:mm:ss'),
+      get: (timestamp) => dayjs(timestamp).format('YYYY-MM-DD HH:mm:ss'),
     },
   },
   {
